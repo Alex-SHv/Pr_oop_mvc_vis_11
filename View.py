5adda48c1db931e70e315153f6276d1145c7615a
Alex-SHv <116091695+Alex-SHv@users.noreply.github.com>
2025-11-25 00:00:54 +0200

    Finish_view_commit

--- View.py
+++ View.py
@@ -1,10 +1,19 @@
-class ManView:
-    def show(self, guess, error, used):
-        print("\nWord: ", "".join(guess))
-        print(f"Errors ({error}): {','.join(used)}")
+﻿class HangmanView:
+    def show_state(self, guessed, errors, used):
+        print("\nСлово: ", " ".join(guessed))
+        print(f"Ошибки ({errors}): {', '.join(used)}")
 
-    def inp_letter(self):
-        return input("Letter: ").lower()
+    def ask_letter(self):
+        return input("Буква: ").lower()
 
-    def show(self, text):
-        print("There is such a letter")
+    def show_result(self, text):
+        print(text)
+
+    def show_correct(self):
+        print("Есть такая буква!")
+
+    def show_wrong(self):
+        print("Нет такой буквы!")
+
+    def show_already(self):
+        print("Эта буква уже была!")
