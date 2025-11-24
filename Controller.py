aaa4551cb20f2d4b642a54528a74c678fc9f9578
Alex-SHv <116091695+Alex-SHv@users.noreply.github.com>
2025-11-24 23:59:21 +0200

    Finish_controller_commit

--- Controller.py
+++ Controller.py
@@ -1,28 +1,61 @@
-from Model import ManModel
-from View import ManView
+﻿from Model import HangmanModel
+from View import HangmanView
 
-class ManController:
+class HangmanController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
 
-    def letter(self, letter):
-        if letter in self.letters:
+    
+    def check_letter(self, letter):
+        if letter in self.model.used_letters:
             return None 
 
-        self.letters.append(letter)
+        self.model.used_letters.append(letter)
 
-        if letter in self.word:
-            for i, ch in enumerate(self.word):
+        if letter in self.model.word:
+            for i, ch in enumerate(self.model.word):
                 if ch == letter:
-                    self.guessed[i] = letter
+                    self.model.guessed[i] = letter
             return True
         else:
-            self.errors += 1
+            self.model.errors += 1
             return False
 
-    def win(self):
-        return "" not in self.guessed
+    def is_win(self):
+        return "_" not in self.model.guessed
 
-    def lose(self):
-        return self.errors >= self.max_errors
+    def is_lose(self):
+        return self.model.errors >= self.model.max_errors
+
+    def game_loop(self):
+        while True:
+            self.view.show_state(self.model.guessed, self.model.errors, self.model.used_letters)
+
+            letter = self.view.ask_letter()
+
+            result = self.check_letter(letter)
+
+            if result is None:
+                self.view.show_already()
+                continue
+            elif result:
+                self.view.show_correct()
+            else:
+                self.view.show_wrong()
+
+            if self.is_win():
+                self.view.show_state(self.model.guessed, self.model.errors, self.model.used_letters)
+                self.view.show_result("\n Ура !!!  Вы угадали слово!")
+                break
+
+            if self.is_lose():
+                self.view.show_result(f"\n Висилица @ загадоное слово: {self.model.word}")
+                break
+
+
+model = HangmanModel()
+view = HangmanView()
+controller = HangmanController(model, view)
+
+controller.game_loop()
